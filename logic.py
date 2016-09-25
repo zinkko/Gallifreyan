8c54f5571d3cdbc043df4fa8e9fcf5a9e04394fb
Ilari Sinkkonen <sinkkonenilari@gmail.com>
2016-09-25 19:00:42 +0300

    circle creation and fixed images not being centered

--- logic.py
+++ logic.py
@@ -1,8 +1,24 @@
-class Logic(object):
+from shapes import *
 
+from math import hypot
+
+class Logic(object):
 
     def __init__(self, objects = None):
         self.objects = objects or []
+        self.new_object = None
 
     def clear_all(self):
         self.objects = []
+
+    def start_draw(self, x, y):
+        self.new_object = Circle(x, y, 0)
+        print("create circle", x, y)
+        self.objects.append(self.new_object)
+
+    def notify_motion(self, x, y):
+        if self.new_object is None:
+            return
+        ox, oy, _ = self.new_object.params
+        self.new_object.r = hypot(abs(ox-x), abs(oy-y))
+        self.new_object.params = (ox, oy, self.new_object.r)
--- ui/ui.py
+++ ui/ui.py
@@ -24,7 +24,10 @@ class UI(Gtk.Window):
 
         self.canvas = Gtk.DrawingArea()
         self.canvas.connect("draw", self.on_draw)
-        self.canvas.set_events(Gdk.EventMask.BUTTON_PRESS_MASK | Gdk.EventMask.BUTTON_RELEASE_MASK)
+        self.canvas.set_events(Gdk.EventMask.BUTTON_PRESS_MASK
+                                | Gdk.EventMask.BUTTON_RELEASE_MASK
+                                | Gdk.EventMask.BUTTON1_MOTION_MASK)
+        self.canvas.connect("motion-notify-event", self.on_drag)
         self.canvas.connect("button-press-event", self.on_button_press)
         self.canvas.connect("button-press-event", self.on_button_release)
         self.box.pack_start(self.canvas, expand=True, fill=True, padding=0)
@@ -43,27 +46,41 @@ class UI(Gtk.Window):
         self.connect('delete-event', Gtk.main_quit)
         self.show_all()
 
+    def map_coordinates(self, x, y):
+        win_x, win_y, width, height = self.canvas.get_window().get_geometry()
+        scale = min(width, height) / 2
+        x = (x - win_x - width/2) / scale
+        y = -1 * (y - win_y - height/2) / scale
+        return (x,y)
 
     def on_draw(self, wid, context):
 
         #context.set_source_rgb(0.33, 0.02, 0.26)
         #context.set_line_width(1.4)
 
-        w,h = self.get_size()
+        _,_,w,h = self.canvas.get_window().get_geometry()
         context.translate(w/2, h/2)
 
         context.scale(1,-1)
 
         self.painter.paint(context, h, self.logic.objects)
 
-    def on_button_press(self, wid, e):
-        pass
+    def on_button_press(self, widget, event):
+        _, x, y, _ = event.window.get_pointer()
+        x,y = self.map_coordinates(x,y)
+        self.logic.start_draw(x,y)
 
     def on_button_release(self, wid, event):
         pass
 
     def on_key_press(self, widget, event):
         pass
+
+    def on_drag(self, widget, event):
+        _, x, y, _ = event.window.get_pointer()
+        x,y = self.map_coordinates(x,y)
+        self.logic.notify_motion(x,y)
+        self.queue_draw()
 
     def start(self):
         signal(SIGINT, SIG_DFL)
